128df6842851543f708dac15cc96d2b2e1baeb86
Joshua Mabawonku <drakejoshua086@gmail.com>
2023-12-30 03:58:50 +0100

    feat: added to-do functionalities
    - added the "create task" functionality
    - added the "delete task" functionality
    
    N.B. all these make the desktop version work well... sorting hasn't
    been fully finished at this point

--- script.js
+++ script.js
@@ -1,1 +1,164 @@
-console.log("hello");
+// create a tasks array to store all the initial and created tasks
+var tasksArray = [
+    {
+        id: 1,
+        text: "Brush Teeth"
+    },
+    {
+        id: 2,
+        text: "lay bed"
+    },
+    {
+        id: 3,
+        text: "think it"
+    },
+    {
+        id: 4,
+        text: "put a ding"
+    },
+    {
+        id: 5,
+        text: "motivate yourself"
+    },
+    {
+        id: 6,
+        text: "sing out loud"
+    }
+]
+
+
+// import the DOM representations of element on the page
+var tasksList = document.getElementById("tasks-list"), addButton = document.getElementById("create-task-btn"), 
+    createTaskInput = document.getElementById("create-task-input");
+
+
+// create a stateful representation of the to-do section
+var ToDoSectionStatefulRepresentation = new StatefulElement( 
+    document.getElementById("to-do"),
+
+    // initial state
+    {
+        state: "not-empty",
+        tasks: tasksArray
+    },
+
+    // state listener function
+    function( internalState, element ) {
+
+        // check the state based on the internalState and display the appropriate UI
+        switch ( internalState.state ) {
+            case "empty":
+                element.classList.remove("empty", "not-empty");
+                element.classList.add("empty");
+            break;
+            
+            case "not-empty":
+
+                // check if the tasks in this state is not empty if not, revert back to former state
+                if ( internalState.tasks.length > 0 ) {
+
+                    element.classList.remove("empty", "not-empty");
+                    element.classList.add("not-empty");
+    
+                    // clear the tasksList of all previous elements
+                    tasksList.innerHTML = "";
+    
+                    // make the tasksList a sortable
+                    var sortable = Sortable.create( tasksList, {
+                        swap: true,
+                        swapClass: "swap-class",
+                        animation: 300,
+                        onUpdate: function( event ) {
+                            // updateSortInTasksArray( event.oldIndex, event.newIndex );
+                        }
+                    } );
+    
+                    // insert all the task in this state into the tasksList as <li> elements
+                    for ( var task of internalState.tasks ) {
+                        tasksList.innerHTML += `
+                            <li class="task" data-todoid="${task.id}"> 
+                                <span class="task-name">${task.text}</span>
+                
+                                <span class="task-icon material-symbols-rounded" onclick="deleteTask(this.parentElement)">
+                                    delete
+                                </span>
+                            </li>
+                        `;
+                    }
+                } else {
+                    element.statefulRepresentation.internalState = { state: "empty" };
+                }
+            break;
+        }
+    }
+)
+
+
+// the deleteTask() function
+// this function is called to the delete a task from the tasksList and tasksArray using the delete icon on the page
+function deleteTask( task ){
+    // get the data-todoid attr value from the <li>.task element
+    var id = task.getAttribute("data-todoid");
+
+    // remove the <li> element from the page
+    task.remove();
+
+    // get the index of the task object with the correlating data-todoid value from the tasksArray
+    var taskIndexInArray = tasksArray.findIndex( function( value ) {
+        return ( value.id == id );
+    });
+
+    // check if returned index is valid if so, remove the task object from the tasksArray
+    // , re-map the id's of the task object in the tasksArray to be orderly and re-display the tasksList
+    if ( taskIndexInArray != -1 ) {
+        tasksArray.splice( taskIndexInArray, 1 );
+
+        var id = 0;
+
+        var newTasksArray = tasksArray.map( 
+            function( value ) {
+                id++;
+
+                return {
+                    id: id,
+                    text: value.text
+                }
+            }
+        )
+
+        tasksArray = newTasksArray;
+
+        ToDoSectionStatefulRepresentation.internalState = { state: "not-empty", tasks: tasksArray };
+    }
+}
+
+
+// the addTask() function
+// this function is called to add a new task using the task objective from the input on the page
+function addTask() {
+
+    // get the newTasktext from the createTaskInput on the page
+    var newTaskText = createTaskInput.value;
+
+    // check if the newTaskText is not empty if so, create a new task object and add it to the tasksArray
+    // and also re-display the tasksList on the page with the updated tasksArray( using the stateful representation )
+    if ( newTaskText != "" ) {
+
+        // push the new task object to the tasksArray
+        tasksArray.push(
+            {
+                id: ( tasksArray.length + 1 ),
+                text: newTaskText
+            }
+        );
+
+
+        // update the state of the ToDoSection
+        ToDoSectionStatefulRepresentation.internalState = {
+            state: "not-empty",
+            tasks: tasksArray
+        }
+    } else {
+        alert("Cannot create task with no objective");
+    }
+}
